97219704ef1278020dfd16aff907cf896ff3c8f2
Kamil Tomasz Matyjaszczyk <667037@stud.hvl.no>
2024-10-01 22:35:14 +0200

    grap 4

--- GPU.js
+++ GPU.js
@@ -8,12 +8,12 @@ GPU = {
     _scrn: {},
 
     reset: function () {
-    var c = document.getElementById('screen');
+    var c = document.getElementById('screen'); // Henter canvas-elementet med ID 'screen'
     if(c && c.getContext){
-        GPU._canvas = c.getContext('2d');
+        GPU._canvas = c.getContext('2d');// Får 2D-tegnekonteksten for canvaset
         if(GPU._canvas) {
             if(GPU._canvas.createImageData) GPU._scrn = GPU._canvas.createImageData(160, 144);
-            else if (GPU._canvas.getImageData) GPU._scrn = GPU._canvas.getImageData(0, 0, 160, 144);
+            else if (GPU._canvas.getImageData) GPU._scrn = GPU._canvas.getImageData(0, 0, 160, 144);// Hvis createImageData ikke er tilgjengelig, sjekker om getImageData finnes
             else GPU._scrn ={
                 'width': 160,
                 'height': 144,
@@ -26,7 +26,45 @@ GPU = {
     }
     },
     step: function () {
+        GPU._modeclock += Z80._r.t;
+        switch(GPU._mode) {
+            case 2: //OAM
+                if(GPU._modeclock >=80) {
+                    GPU._modeclock = 0;
+                    GPU._mode = 3; //vram
+                } break;
 
+            case 3:
+                if(GPU._modeclock >=172) {
+                    GPU._modeclock = 0;
+                    GPU._mode = 0; //H-blank
+                    GPU.renderscan();  // Rendre scanlinen til pikselbufferen
+                } break;
+
+            case 0: //hblank
+                if(GPU._modeclock >= 204) {
+                    GPU._modeclock = 0;
+                    GPU._line++;
+                    if(GPU._line == 143) {
+                        GPU._mode = 1;// vblank
+                        GPU._canvas.putImageData(GPU._scrn, 0, 0);
+                    } else {
+                        GPU._mode = 2;
+                    }
+                } break;
+
+            case 1: //vblank
+                if(GPU._modeclock >= 456) {
+                    GPU._modeclock = 0;
+                    GPU._line++;
+                    if(GPU._line > 153) {
+                        // Restart
+                        GPU._mode = 2;
+                        GPU._line = 0;
+                    }
+                } break;}},
+
+    renderscan: function () {
 
     },
 }
