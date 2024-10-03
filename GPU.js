233bc60b0a91c7f9dd9ac750b82a9e719d39f272
Kamil Tomasz Matyjaszczyk <667037@stud.hvl.no>
2024-10-03 23:49:24 +0200

    grap 4 quickfix

--- GPU.js
+++ GPU.js
@@ -6,9 +6,11 @@ GPU = {
     _line: 0,
     _canvas: [],
     _scrn: {},
+    _tileset: [],
 
     reset: function () {
     var c = document.getElementById('screen'); // Henter canvas-elementet med ID 'screen'
+    GPU._tileset = [];
     if(c && c.getContext){
         GPU._canvas = c.getContext('2d');// Får 2D-tegnekonteksten for canvaset
         if(GPU._canvas) {
@@ -22,6 +24,14 @@ GPU = {
             for(var i=0; i< 160*144*4; i++)
                 GPU._scrn.data[i] = 255;
             GPU._canvas.putImageData(GPU._scrn, 0, 0);
+        }
+        for(var i = 0; i < 384; i++)
+        {
+            GPU._tileset[i] = [];
+            for(var j = 0; j < 8; j++)
+            {
+                GPU._tileset[i][j] = [0,0,0,0,0,0,0,0];
+            }
         }
     }
     },
@@ -63,6 +73,20 @@ GPU = {
                         GPU._line = 0;
                     }
                 } break;}},
+
+    updatetile: function (addr, val ) {
+        addr &= 0x1FFE; // avgrenser riktig område
+        var tile = (addr >> 4) & 511;
+        var y = (addr >> 1) & 7;
+        var sx;
+        for(var x = 0; x < 8; x++)
+        {
+            sx = 1 << (7-x);
+            GPU._tileset[tile][y][x] =
+                ((GPU._vram[addr] & sx)   ? 1 : 0) +
+                ((GPU._vram[addr+1] & sx) ? 2 : 0);
+        }
+    },
 
     renderscan: function () {
 
